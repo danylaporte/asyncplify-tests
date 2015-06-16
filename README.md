230a4b93e1d34df0826c8033e4df5e71625b3054
danylaporte <dany_laporte@hotmail.com>
2015-06-16 17:15:00 -0400

    Implementation

--- README.md
+++ README.md
@@ -7,7 +7,54 @@ Common tests for asyncplify operators
 npm install asyncplify-tests
 ```
 
+Execute tests on a asyncplify source
+```js
+var tests = require('asyncplify-tests');
+	
+describe('fromArray' function () {
+	asyncplify
+	    .fromArray([0, 1])
+	    .pipe(tests.itShouldClose())
+	    .pipe(tests.itShouldEndSync())
+	    .pipe(tests.itShouldEmitValues([0, 1]));
+});
+```
+
 ## Documentation
+
+### itShouldClose
+Test if the source support closing during iteration.
+
+```js
+asyncplify
+	.fromArray([0, 1])
+	.pipe(tests.itShouldClose());
+```
+
+### itShouldEmitValues
+Test if the source emit the specified values.
+```js
+asyncplify
+	.fromArray([0, 1])
+	.pipe(tests.itShouldEmitValues([0, 1]));
+```
+
+### itShouldEndAsync
+Test if the source end asynchronously.
+```js
+asyncplify
+	.fromArray([0, 1])
+	.pipe(tests.itShouldEndAsync());
+// Not OK! fromArray is synchronous
+```
+
+### itShouldEndSync
+Test if the source end synchronously.
+```js
+asyncplify
+	.fromArray([0, 1])
+	.pipe(tests.itShouldEndSync());
+```
 
 ## License
 The MIT License (MIT)
---
+++ index.js
@@ -0,0 +1,94 @@
+var assert = require('assert');
+var should = require ('should');
+
+module.exports.itShouldClose = function (options) {
+	return function (source) {
+		it(options && options.title || 'should close', function () {
+			source.subscribe().close();
+		});
+
+		return source;
+	};
+};
+
+module.exports.itShouldEmitValues = function (options) {
+	return function (source) {
+		var expectedValues = Array.isArray(options)
+			? options
+			: options && options.values;
+
+		it(options && options.title || 'should emit values', function (done) {
+			var array = [];
+
+			source.subscribe({
+				emit: function (value) {
+					array.push(value);
+				},
+				end: function () {
+
+					if (expectedValues)
+						array.should.eql(expectedValues);
+
+					else if (options && options.expectedCount)
+						array.length.should.equal(options.expectedCount);
+
+					done();
+				}
+			});
+
+			return source;
+		});
+	};
+};
+
+module.exports.itShouldEndAsync = function (options) {
+	return function (source) {
+		it('should end asynchronously', function (done) {
+			var called = false;
+
+			source.subscribe({
+				end: function (err) {
+					if (options && options.error)
+						assert(err !== null);
+					else
+						assert(err === null);
+
+					called = true;
+					setTimeout(done, 0);
+				}
+			});
+
+			called.should.equal(false);
+		});
+
+		return source;
+	};
+};
+
+module.exports.itShouldEndSync = function (options) {
+	return function (source) {
+		it('should end synchronously', function (done) {
+			var called = false;
+
+			source.subscribe({
+				end: function (err) {
+					if (options && options.error) {
+						if (typeof options.error === 'string')
+							options.error.should.equal(err);
+						else
+							assert(err !== null);
+					} else {
+						assert(err === null);
+					}
+
+					called = true;
+				}
+			});
+
+			called.should.equal(true);
+			done();
+		});
+		
+		return source;
+	};
+};
